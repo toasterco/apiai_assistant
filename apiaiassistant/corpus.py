3b36fee74f0680c9d3aef473a4ab7ff553bbd2ea
Zack-- <zdibe@google.com>
2017-07-24 15:53:53 +0100

    [ASS-109] fragment corpus in parts to support confirmations and suggestions

--- apiaiassistant/corpus.py
+++ apiaiassistant/corpus.py
@@ -14,9 +14,13 @@ class Corpus(object):
         filepath (str): path to the json file
     """
 
+    DEFAULT_CONFIRMATIONS = [['Yes', 'No']]
+
     def __init__(self, filepath):
         self.filepath = filepath
         self.corpus = None
+        self.suggestions = None
+        self.confirmations = None
         self.init_corpus()
 
     def __contains__(self, x):
@@ -29,12 +33,29 @@ class Corpus(object):
             self.init_corpus()
         return x in self.corpus
 
+    def validate(self, data):
+        """
+        Args:
+            data (:obj:`dict`): JSON data to validate
+        Returns:
+            bool: True if data is valid/ False otherwise
+        """
+
+        return 'corpus' in data
+
     def init_corpus(self):
         """ Initialize the corpus by reading the json file and loading
         it in memory """
 
         with open(self.filepath, 'r') as fd:
-            self.corpus = json.load(fd)
+            data = json.load(fd)
+            if not self.validate(data):
+                raise ValueError('{} not a corpus'.format(self.filepath))
+
+            self.corpus = data['corpus']
+            self.suggestions = data.get('suggestions')
+            self.condirmations = data.get(
+                'confirmations', self.DEFAULT_CONFIRMATIONS)
 
     def __getitem__(self, key):
         """
@@ -45,7 +66,12 @@ class Corpus(object):
 
         return self.get(key)
 
-    def get(self, key):
+    def get_confirmation(self):
+        """ Returns a random confirmation """
+
+        return random.choice(self.confirmations)
+
+    def get(self, key, source=None):
         """
         Returns:
             str: A randomly selected element of the key item in the corpus.
@@ -55,7 +81,10 @@ class Corpus(object):
         if self.corpus is None:
             self.init_corpus()
 
-        if key not in self.corpus:
+        if source is None:
+            source = self.corpus
+
+        if key not in source:
             return None
 
-        return random.choice(self.corpus[key])
+        return random.choice(source[key])
